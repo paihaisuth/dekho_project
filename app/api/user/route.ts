c19f12955ee9449f3449afabdb65886165d0a145
kasaje <kj.kaseamsankeawjeen@gmail.com>
2025-11-18 23:14:45 +0700

    feat: implement create user API

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,9 +1,10 @@
-import { generateAPIResponse } from "@/app/utils/function";
+import { generateAPIResponse, getBody } from "@/app/utils/function";
 import { middleware } from "@/middleware";
 import { UserRepository } from "@/repositories/userRepository";
 import { UserService } from "@/services/userService";
 import { CustomError } from "@/utils/customError";
 import { NextRequest } from "next/server";
+import { IbodyRegister } from "../auth/register/route";
 
 export const GET = async (req: NextRequest) => {
   try {
@@ -20,6 +21,50 @@ export const GET = async (req: NextRequest) => {
     return generateAPIResponse({ items: userList }, 200);
   } catch (error) {
     console.log("========== ERROR GET USER ========== ", error);
+    if (error instanceof CustomError)
+      return generateAPIResponse({ message: error.message }, error.status);
+    return generateAPIResponse(
+      {
+        message:
+          (error as { message: string }).message || "Internal Server Error",
+      },
+      (error as { status: number }).status || 500
+    );
+  }
+};
+
+export const POST = async (req: NextRequest) => {
+  try {
+    console.log("========== START CREATE USER ========== ");
+
+    await middleware(req);
+
+    const {
+      email,
+      firstname,
+      lastname,
+      password,
+      phoneNumber,
+      roleID,
+      username,
+    } = await getBody<IbodyRegister>(req);
+
+    const userRepository = new UserRepository();
+    const userService = new UserService(userRepository);
+
+    await userService.createUser({
+      email,
+      firstname,
+      lastname,
+      password,
+      phoneNumber,
+      roleID,
+      username,
+    });
+
+    console.log("========== END CREATE USER ========== ");
+    return generateAPIResponse({ message: "User created successfully" }, 201);
+  } catch (error) {
     if (error instanceof CustomError)
       return generateAPIResponse({ message: error.message }, error.status);
     return generateAPIResponse(
