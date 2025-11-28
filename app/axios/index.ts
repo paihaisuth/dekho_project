b8d938cce25da46510c89bdcca8e99f728f87c5d
kasaje <kj.kaseamsankeawjeen@gmail.com>
2025-11-28 10:35:16 +0700

    feat: provider connect API repair request

--- app/axios/index.ts
+++ app/axios/index.ts
@@ -1,5 +1,6 @@
 import authQuery from "./authQuery";
 import roleQuery from "./roleQuery";
 import dormitoryQuery from "./dormitoryQuery";
+import repairRequestQuery from "./repairRequestQuery";
 
-export { authQuery, roleQuery, dormitoryQuery };
+export { authQuery, roleQuery, dormitoryQuery, repairRequestQuery };
---
+++ app/axios/repairRequestQuery.ts
@@ -0,0 +1,54 @@
+import IrepairRequest from "@/schema/RepairRequest";
+import { getError, getResponse } from "../utils/function";
+import { IpaginationFormat } from "../utils/interface";
+import axiosInstance from "./instance";
+
+const repairRequestQuery = {
+  async list(userID: string, page: number, pageSize: number = 10) {
+    try {
+      const response = await axiosInstance.get(`/repairRequest`, {
+        params: {
+          userID,
+          page,
+          pageSize,
+        },
+      });
+      return getResponse<IpaginationFormat<IrepairRequest>>(response);
+    } catch (error) {
+      return getError<IpaginationFormat<IrepairRequest>>(error);
+    }
+  },
+  async getByID(repairRequestID: string) {
+    try {
+      const response = await axiosInstance.get(
+        `/repairRequest/${repairRequestID}`
+      );
+      return getResponse<IrepairRequest>(response);
+    } catch (error) {
+      return getError<IrepairRequest>(error);
+    }
+  },
+  async update(repairRequestID: string, data: Partial<IrepairRequest>) {
+    try {
+      const response = await axiosInstance.put(
+        `/repairRequest/${repairRequestID}`,
+        data
+      );
+      return getResponse<IrepairRequest>(response);
+    } catch (error) {
+      return getError<IrepairRequest>(error);
+    }
+  },
+  async delete(repairRequestID: string) {
+    try {
+      const response = await axiosInstance.delete(
+        `/repairRequest/${repairRequestID}`
+      );
+      return getResponse<IrepairRequest>(response);
+    } catch (error) {
+      return getError<IrepairRequest>(error);
+    }
+  },
+};
+
+export default repairRequestQuery;
